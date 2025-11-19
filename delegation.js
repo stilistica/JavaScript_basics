103d8d78e24d305c3dd6bc577e89f699335814dc
stillistica <rozhkovavika24@gmail.com>
2025-11-19 22:17:36 +0200

    add links

--- delegation.js
+++ delegation.js
@@ -1,1 +1,4 @@
 // https://uk.javascript.info/event-delegation
+// https://www.youtube.com/watch?v=1lcM6MS1X1c
+// https://www.youtube.com/watch?v=ydnfxSf45MY
+// https://www.youtube.com/watch?v=Pl2xy0W38Mo
